aca21f27ae8cd1986012c26efc7e11c550076a81
faruk alam <farukinnfo@gmail.com>
2025-09-02 11:16:28 +0200

    file completed

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/home', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, './public/about.html')));
+app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, './public/contact.html')));
+
+// Start server
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
